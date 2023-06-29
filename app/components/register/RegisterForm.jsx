92c0ea935b84e0b8debf5c2e634d52c510279f8d
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2023-06-29 18:47:02 +0530

    [Feature] Registering a new User

---
+++ app/components/register/RegisterForm.jsx
@@ -0,0 +1,43 @@
+"use client";
+import SignUpButton from "@/components/ui/buttons/PrimaryButton";
+import React from "react";
+import Link from "next/link";
+import EmailField from "@/components/ui/inputFields/EmailField";
+import PasswordField from "@/components/ui/inputFields/PasswordField";
+
+const RegisterForm = () => {
+
+    const [signUpDetails, setSignUpDetails] = React.useState({
+        email: "",
+        password: "",
+    });
+
+    return (
+        <form>
+            <div className="flex flex-col w-full gap-[20px]">
+                <EmailField
+                    id="email"
+                    value={signUpDetails.email}
+                    onChange={(e) =>
+                    setSignUpDetails({ ...signUpDetails, email: e.target.value })
+                    }
+                    label="Email"
+                />
+                <PasswordField 
+                    id="password"
+                    value={signUpDetails.password}
+                    onChange={(e) =>
+                    setSignUpDetails({ ...signUpDetails, password: e.target.value })
+                    }
+                    label="Password" 
+                />
+            </div>
+            <div className="w-full flex flex-col gap-[11px]">
+                <SignUpButton type="submit">Sign Up With Email</SignUpButton>
+                <p className="f14 fw700 lh22 text-[#828282]">By continuing, i agree to the <Link href="#" className="text-[#3B2273]">terms of service.</Link></p>
+            </div>
+        </form>
+    );
+};
+
+export default RegisterForm;
---
+++ app/components/register/register.jsx
@@ -0,0 +1,31 @@
+import Link from "next/link"
+import RegisterForm from "./RegisterForm"
+
+const Register = () => {
+    return (
+        <section className={styles.formContainer}>
+            <div className="px-[37px] py-[32px]">
+                <h1 className="h3 text-[#13013C] w-[412px]">Create your profile, get rated, and <span className="text-[#FFBB34]">start playing!</span></h1>
+                <RegisterForm />
+
+                <div className="flex flex-col gap-[106px]">
+                    <div className="flex items-center justify-between">
+                    <p className="text-center f16 fw700 lh24 text-[#13013C]">Register With :</p>
+                        <div className="flex gap=[8px] items-center">
+                            <button className="h-[50px] w-[50px] rounded-sm shadow-md">G</button>
+                            <button className="h-[50px] w-[50px] rounded-sm shadow-md">F</button>
+                        </div>
+                    </div>
+                    <p className="text-center f18 fw400 lh30 text-[#828282]">Already have an account? 
+                    <Link href={"/signin"} className="text-[#13013C]">Sign In</Link></p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+const styles = {
+    formContainer: "",
+    formHeader: "h4 text-[#13013C]",
+}
+export default Register
