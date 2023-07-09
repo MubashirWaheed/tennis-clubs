ce96e1440c8cf134767448a3f7cd97b7462c0322
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2023-07-10 00:49:21 +0530

    [smallUITweaks] small size button added

--- components/ui/buttons/PrimaryButton.jsx
+++ components/ui/buttons/PrimaryButton.jsx
@@ -1,13 +1,42 @@
 import Image from "next/image";
 import rightArrowIcon from "../assets/rightArrow.svg";
 
-const PrimaryButton = ({children, type, onClick}) => {
+const PrimaryButton = ({ size, children, type, onClick }) => {
     return (
-        <button className="px-[20px] py-[13px] rounded-[8px] inline-flex items-center justify-center gap-[20px] bg-[#027333] border-2 border-[#027333] cursor-pointer"  type={type} onClick={onClick}>
-            <span className="p f16 lh24 text-white">{children}</span>
-            <span className="w-[20px] h-[20px]"><Image src={rightArrowIcon} alt="Right Arrow Icon" className="w-[20px] h-[20px]"/></span>
-        </button>
-    )
-}
+        <>
+        {size === "small" ? (
+            <button
+            className="px-[10px] py-[8px] rounded-[8px] inline-flex items-center justify-center gap-[20px] bg-[#027333] border-2 border-[#027333] cursor-pointer"
+            type={type}
+            onClick={onClick}
+            >
+                <span className="p f14 lh24 fw700 text-white">{children}</span>
+                <span className="w-[20px] h-[20px]">
+                    <Image
+                    src={rightArrowIcon}
+                    alt="Right Arrow Icon"
+                    className="w-[20px] h-[20px]"
+                    />
+                </span>
+            </button>
+        ) : (
+            <button
+            className="px-[20px] py-[13px] rounded-[8px] inline-flex items-center justify-center gap-[20px] bg-[#027333] border-2 border-[#027333] cursor-pointer"
+            type={type}
+            onClick={onClick}
+            >
+                <span className="p f16 lh24 fw700 text-white">{children}</span>
+                <span className="w-[20px] h-[20px]">
+                    <Image
+                    src={rightArrowIcon}
+                    alt="Right Arrow Icon"
+                    className="w-[20px] h-[20px]"
+                    />
+                </span>
+            </button>
+        )}
+        </>
+    );
+};
 
-export default PrimaryButton
+export default PrimaryButton;
--- components/ui/buttons/SecondaryButton.jsx
+++ components/ui/buttons/SecondaryButton.jsx
@@ -1,12 +1,42 @@
 import Image from "next/image";
 import greenArrowIcon from "../assets/greenArrow.svg";
-const SecondaryButton = ({children, type, onClick}) => {
-    return (
-        <button className="border-2 border-[#027333] px-[20px] py-[13px] rounded-[8px] inline-flex justify-center items-center gap-[20px] bg-[#FFFFFF] cursor-pointer" type={type} onClick={onClick}>
-            <span className="p f16 lh24 text-[#027333]">{children}</span>
-            <span className="w-[20px] h-[20px]"><Image src={greenArrowIcon} alt="Right Arrow Icon" className="w-[20px] h-[20px]"/></span>
-        </button>
-    )
-}
 
-export default SecondaryButton
+const SecondaryButton = ({ size, children, type, onClick }) => {
+    return (
+        <>
+        {size === "small" ? (
+            <button
+            className="border-2 border-[#027333] px-[10px] py-[8px] rounded-[8px] inline-flex justify-center items-center gap-[20px] bg-[#FFFFFF] cursor-pointer"
+            type={type}
+            onClick={onClick}
+            >
+            <span className="p f16 lh24 fw700 text-[#027333]">{children}</span>
+            <span className="w-[20px] h-[20px]">
+                <Image
+                src={greenArrowIcon}
+                alt="Right Arrow Icon"
+                className="w-[20px] h-[20px]"
+                />
+            </span>
+            </button>
+        ) : (
+            <button
+            className="border-2 border-[#027333] px-[20px] py-[13px] rounded-[8px] inline-flex justify-center items-center gap-[20px] bg-[#FFFFFF] cursor-pointer"
+            type={type}
+            onClick={onClick}
+            >
+            <span className="p f16 lh24 fw700 text-[#027333]">{children}</span>
+            <span className="w-[20px] h-[20px]">
+                <Image
+                src={greenArrowIcon}
+                alt="Right Arrow Icon"
+                className="w-[20px] h-[20px]"
+                />
+            </span>
+            </button>
+        )}
+        </>
+    );
+};
+
+export default SecondaryButton;
