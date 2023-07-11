f4c09590dd55f1f0a5cecdeb72dcb0ed564c10d6
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2023-07-11 11:55:11 +0530

    [changes] Social Icons Added in signup and register

--- app/(authentication)/register/page.jsx
+++ app/(authentication)/register/page.jsx
@@ -1,28 +1,52 @@
 import Link from "next/link"
 import RegisterForm from "./components/RegisterForm"
+import Image from "next/image";
 
 const RegisterAsAPlayer = () => {
-    return(
-        <section className={styles.formContainer}>
-            <div className="px-[37px] py-[32px] w-full flex flex-col gap-[30px]">
-                <h1 className="h3 text-[#13013C] w-[412px]">Create your profile, get rated, and <span className="h3 text-[#FFBB34]">start playing!</span></h1>
-                
-                <RegisterForm />
+    return (
+      <section className={styles.formContainer}>
+        <div className="px-[37px] py-[32px] w-full flex flex-col gap-[30px]">
+          <h1 className="h3 text-[#13013C] w-[412px]">
+            Create your profile, get rated, and{" "}
+            <span className="h3 text-[#FFBB34]">start playing!</span>
+          </h1>
 
-                <div className="flex flex-col gap-[106px]">
-                    <div className="flex items-center justify-between">
-                    <p className="text-center f16 fw700 lh24 text-[#13013C]">Register With :</p>
-                        <div className="flex gap=[8px] items-center">
-                            <button className="h-[50px] w-[50px] rounded-sm shadow-md">G</button>
-                            <button className="h-[50px] w-[50px] rounded-sm shadow-md">F</button>
-                        </div>
-                    </div>
-                    <p className="text-center f18 fw400 lh30 text-[#828282]">Already have an account? 
-                    <Link href={"/signin"} className="text-[#13013C]">Sign In</Link></p>
-                </div>
+          <RegisterForm />
+
+          <div className="flex flex-col gap-[106px]">
+            <div className="flex items-center justify-between">
+              <p className="text-center f16 fw700 lh24 text-[#13013C]">
+                Register With :
+              </p>
+              <div className="flex gap=[8px] items-center">
+                <button className="h-[50px] w-[50px] rounded-sm shadow-md flex items-center justify-center">
+                  <Image
+                    src="/google.png"
+                    alt="Social Icons"
+                    width={30}
+                    height={30}
+                  />
+                </button>
+                <button className="h-[50px] w-[50px] rounded-sm shadow-md flex items-center justify-center">
+                  <Image
+                    src="/facebook.png"
+                    alt="Social Icons"
+                    width={30}
+                    height={30}
+                  />
+                </button>
+              </div>
             </div>
-        </section>
-    )
+            <p className="text-center f18 fw400 lh30 text-[#828282]">
+              Already have an account?
+              <Link href={"/signin"} className="text-[#13013C]">
+                Sign In
+              </Link>
+            </p>
+          </div>
+        </div>
+      </section>
+    );
 }
 const styles = {
     formContainer: "rounded-[10px] bg-[#FFFFFF] w-[581px] z-20",
--- app/(authentication)/signin/components/LogIn.jsx
+++ app/(authentication)/signin/components/LogIn.jsx
@@ -1,27 +1,50 @@
 import Link from "next/link"
 import LogInForm from "./LogInForm"
+import Image from "next/image";
 
 const LogIn = () => {
     return (
-        <section className={styles.formContainer}>
-            <div className="px-[37px] py-[32px] w-full flex flex-col gap-[30px]">
-                <h1 className="h3 text-[#13013C] w-[412px]">Log into your <span className="h3 text-[#FFBB34]">account</span></h1>
-                <LogInForm />
+      <section className={styles.formContainer}>
+        <div className="px-[37px] py-[32px] w-full flex flex-col gap-[30px]">
+          <h1 className="h3 text-[#13013C] w-[412px]">
+            Log into your <span className="h3 text-[#FFBB34]">account</span>
+          </h1>
+          <LogInForm />
 
-                <div className="flex flex-col gap-[106px]">
-                    <div className="flex items-center justify-between">
-                    <p className="text-center f16 fw700 lh24 text-[#13013C]">LogIn With :</p>
-                        <div className="flex gap=[8px] items-center">
-                            <button className="h-[50px] w-[50px] rounded-sm shadow-md">G</button>
-                            <button className="h-[50px] w-[50px] rounded-sm shadow-md">F</button>
-                        </div>
-                    </div>
-                    <p className="text-center f18 fw400 lh30 text-[#828282]">Dont have an account? 
-                    <Link href={"/register"} className="text-[#13013C]">Sign Up</Link></p>
-                </div>
+          <div className="flex flex-col gap-[106px]">
+            <div className="flex items-center justify-between">
+              <p className="text-center f16 fw700 lh24 text-[#13013C]">
+                LogIn With :
+              </p>
+              <div className="flex gap=[8px] items-center">
+                <button className="h-[50px] w-[50px] rounded-sm shadow-md flex items-center justify-center">
+                  <Image
+                    src="/google.png"
+                    alt="Social Icons"
+                    width={30}
+                    height={30}
+                  />
+                </button>
+                <button className="h-[50px] w-[50px] rounded-sm shadow-md flex items-center justify-center">
+                  <Image
+                    src="/facebook.png"
+                    alt="Social Icons"
+                    width={30}
+                    height={30}
+                  />
+                </button>
+              </div>
             </div>
-        </section>
-    )
+            <p className="text-center f18 fw400 lh30 text-[#828282]">
+              Dont have an account?
+              <Link href={"/register"} className="text-[#13013C]">
+                Sign Up
+              </Link>
+            </p>
+          </div>
+        </div>
+      </section>
+    );
 }
 
 const styles = {
