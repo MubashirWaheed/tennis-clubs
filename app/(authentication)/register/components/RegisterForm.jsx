6f2c926f66d81ea293455432cce6de4a153605e9
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2023-07-20 19:31:20 +0530

    changes made

--- app/(authentication)/register/components/RegisterForm.jsx
+++ app/(authentication)/register/components/RegisterForm.jsx
@@ -4,8 +4,10 @@ import React from "react";
 import Link from "next/link";
 import EmailField from "@/components/ui/inputFields/EmailField";
 import PasswordField from "@/components/ui/inputFields/PasswordField";
+import { useRouter } from "next/navigation";
 
 const RegisterForm = () => {
+    const router = useRouter();
 
     const [signUpDetails, setSignUpDetails] = React.useState({
         email: "",
@@ -33,7 +35,11 @@ const RegisterForm = () => {
                 />
             </div>
             <div className="w-full flex flex-col gap-[11px]">
-                <SignUpButton type="submit">Sign Up With Email</SignUpButton>
+                <SignUpButton onClick={(event) => {
+                    event.preventDefault();
+                    router.push("/register/profile")
+                }
+                } type="submit">Sign Up With Email</SignUpButton>
                 <p className="f14 fw700 lh22 text-[#828282]">By continuing, i agree to the <Link href="/signin" className="text-[#3B2273]">terms of service.</Link></p>
             </div>
         </form>
