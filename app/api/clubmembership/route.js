31e8620ec44035f1d428e9048e8c22b8810a5a6a
mubashir <mubashir3080@gmail.com>
2023-12-19 17:34:54 +0400

    removed check in club route

--- app/api/clubmembership/route.js
+++ app/api/clubmembership/route.js
@@ -5,10 +5,11 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
   const currentUser = await getCurrentUser();
-  if (!currentUser || !currentUser.id) {
-    return NextResponse.json("user not found");
-    // return NextResponse.error("User ID not found", { status: 400 });
-  }
+
+  // if (!currentUser || !currentUser.id) {
+  //   return NextResponse.json("user not found");
+  //   // return NextResponse.error("User ID not found", { status: 400 });
+  // }
   const { id } = currentUser;
 
   const data = await prisma.clubmembership.findMany({
